6d8265f530164956cd9d13b1308c1a5bdf63048c
vivian347 <vkinyua001@gmail.com>
2022-08-24 15:32:12 +0300

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for dig1 in range(0,8):
+for dig1 in range(0, 8):
     for dig2 in range(dig1+1, 10):
         print("{:d}{:d}".format(dig1, dig2), end=", ")
 print("{:d}{:d}".format(dig1 + 1, dig2))

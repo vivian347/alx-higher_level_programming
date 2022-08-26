b64aa2ba2b138c0d6ab1dd7ff31bd6cd374a5f04
vivian347 <vkinyua001@gmail.com>
2022-08-26 05:32:43 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("0 arguments.")
+elif len(argv) == 2:
+    print("{:d} argument:".format(len(argv) - 1))
+else:
+    print("{:d} arguments:".format(len(argv) - 1))
+for i, s in enumerate(argv):
+    if i > 0:
+        print("{:d}: {:s}".format(i, s))

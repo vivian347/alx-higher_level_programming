6ae094ab031f678041a19388d8077c30490d67a5
vivian347 <vkinyua001@gmail.com>
2022-08-27 16:18:01 +0300

    lists

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in my_list:
+        if idx < 0 and idx > len(my_list):
+            return my_list
+        else:
+            my_list.pop(idx)
+            my_list.insert(idx, element)
+            return my_list

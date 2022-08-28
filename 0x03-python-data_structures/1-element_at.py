95aab8fc98e572d21fc105078be9cb2c1f7023b8
vivian347 <vkinyua001@gmail.com>
2022-08-28 11:57:48 +0300

    retrieve

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in my_list:
-        if idx < 0 or idx > len(my_list):
+        if idx < 0:
+            pass
+        elif idx > len(my_list):
             pass
         else:
             return my_list[idx]

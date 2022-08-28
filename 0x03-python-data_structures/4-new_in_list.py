3cba0d69c19269e2d1454f7ffd3329a38c4dad13
vivian347 <vkinyua001@gmail.com>
2022-08-28 12:29:27 +0300

    copy replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,6 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     for i in new_list:
-        if idx < 0 and idx > len(new_list):
-            return new_list
-        else:
-            new_list.pop(idx)
-            new_list.insert(idx, element)
-            return new_list
+        if 0 <= idx < len(new_list):
+            new_list[idx] = element
+        return (new_list)

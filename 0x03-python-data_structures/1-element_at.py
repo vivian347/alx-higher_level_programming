14e1ad6d4e1c38c02e17b91ca2c7166b535bfbe1
vivian347 <vkinyua001@gmail.com>
2022-08-28 12:19:53 +0300

    replace

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0:
-            pass
-        elif idx > len(my_list):
-            pass
-        else:
-            return my_list[idx]
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)

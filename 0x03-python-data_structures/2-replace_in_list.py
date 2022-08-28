b1a597de727035ff2a0305c143442e0b7e019a85
vivian347 <vkinyua001@gmail.com>
2022-08-28 12:07:56 +0300

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,5 @@ def replace_in_list(my_list, idx, element):
         if idx < 0 and idx > len(my_list):
             return my_list
         else:
-            my_list.pop(idx)
-            my_list.insert(idx, element)
+            my_list[idx] = element
             return my_list

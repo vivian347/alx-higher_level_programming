76eb2c6540d2e197dbeca6593eaf5acbabbc6ae9
vivian347 <vkinyua001@gmail.com>
2022-09-01 01:33:09 +0300

    corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if bool(a_dictionary)  == False:
+    if bool(a_dictionary) is False:
         return None
     else:
         best_key = max(a_dictionary, key=a_dictionary.get)

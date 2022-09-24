a8f33c5efe605ba33a4258f61f9a519344d18e81
vivian347 <vkinyua001@gmail.com>
2022-09-24 08:36:26 +0300

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,16 @@
 """creating a class Square"""
 class Square:
     def __init__(self, size=0):
-        """creating instance of size"""
+        """creating instance of size
+        Args:
+            size(int): size of a side of a square
+        Returns:
+            None
+        """
         if type(size) is not int:
-            """ check if size is an integer"""
             raise TypeError("size must be an integer")
         else:
             if size < 0:
-                """ check if size is less 0 """
                 raise ValueError("size must be >= 0")
             else:
-                """ else initialize size to be private """
                 self.__size = size

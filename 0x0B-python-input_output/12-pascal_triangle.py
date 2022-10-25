e2978adbc26403a1e81beed108e9d24c0b1f0369
vivian347 <vkinyua001@gmail.com>
2022-10-25 22:48:18 +0300

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+contains pascal_triangle module
+"""
+
+
+def pascal_triangle(n):
+    my_triangle = []
+
+    if n <= 0:
+        return my_triangle
+    my_triangle = [[1]]
+    for x in range(1, n):
+        m = [1]
+        for y in range(1, x):
+            m.append(my_triangle[x-1][y-1] + my_triangle[x-1][y])
+        m.append(1)
+        my_triangle.append(m)
+    return my_triangle

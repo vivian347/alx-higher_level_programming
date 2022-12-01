cacdf688130fe8e8acecf0fe127adc9f4cd30ea6
vivian347 <vkinyua001@gmail.com>
2022-12-01 20:50:33 +0300

    corr

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,10 @@
 const size = Math.floor(Number(process.argv[2]));
 
 if (isNaN(size)) console.log('Missing size');
-for (let r = 0; r < size; r++) {
-  let row = '';
-  for (let c = 0; c < size; c++) row += 'x';
-  console.log(row);
+else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'x';
+    console.log(row);
+  }
 }

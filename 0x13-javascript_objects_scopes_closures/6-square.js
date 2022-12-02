336388bf0c6897c495083b6aacaaf7b4a56a1fd0
vivian347 <vkinyua001@gmail.com>
2022-12-02 17:06:54 +0300

    class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const square = require('./5-square');
+
+module.exports = class Square extends square {
+  charPrint (c) {
+    if (c === undefined) this.print();
+    else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const x of list) {
+    if (x === searchElement) count += 1;
+  }
+  return count;
+};

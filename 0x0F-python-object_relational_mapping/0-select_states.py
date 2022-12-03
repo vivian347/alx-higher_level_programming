008603904109548f9fadc3db857c30779c937e86
vivian347 <vkinyua001@gmail.com>
2022-12-03 20:14:13 +0300

    0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""  script that lists all states from the database hbtn_0e_0_usa: """
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host = "localhost",
+            user = sys.argv[1],
+            passwd = sys.argv[2],
+            db = sys.argv[3],
+            port = 3306,
+            charset = 'utf8'
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for r in rows:
+        print("({}, '{}')".format(r[0], r[1]))
+

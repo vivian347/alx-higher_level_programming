14a8edad7405e56a715cb02748a115d854cd20d6
vivian347 <vkinyua001@gmail.com>
2022-12-04 11:06:57 +0300

    mm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" contains class definition of state and an instance Base=declarative_base """
+""" contains class definition of state and an instance
+    Base=declarative_base """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects import mysql
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'

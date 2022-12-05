818ec64b1b5389c9be944609acc82bc63c26a490
vivian347 <vkinyua001@gmail.com>
2022-12-06 00:47:54 +0300

    13

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,8 +17,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
-    for r in rows:
-        print(r)
-
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name LIKE '%{}%' ORDER BY id"
                 .format(sys.argv[4]))
     rows = cur.fetchall()
-    for r in rows:
-        print(r)
+    for row in rows:
+        print(row)
 
     cur.close()
     db.close()

21ba2d5cbde8068cd99a149f86481b5c627471e5
vivian347 <vkinyua001@gmail.com>
2023-01-11 02:52:44 -0800

    api

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     data = response.read()
     print('Body response:')
-    print("     - type:", type(data))
-    print('     - content:', data)
-    print('     - utf8 content:', data.decode('utf-8'))
+    print(" - type:", type(data))
+    print(' - content:', data)
+    print(' - utf8 content:', data.decode('utf-8'))
